71cc0cdd778520ea79146fe65928c6bbedde2a75
sar762009 <sar762009@gmail.com>
2024-07-04 12:33:07 -0400

    Initial Commit

---
+++ mytodolist/mytodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  mytodolistApp.swift
+//  mytodolist
+//
+//  Created by Saravanan Saminathan on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct mytodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
